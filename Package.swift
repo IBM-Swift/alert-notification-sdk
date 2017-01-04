bb68779f4b044408b9c3da40494d32b202105479
Jim Avery <jlavery@us.ibm.com>
2017-01-04 10:28:04 -0600

    Removing explicit use of HeliumLogger, only using LoggerAPI from here

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ let package = Package(
     name: "AlertNotifications",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8)
     ]
 )
