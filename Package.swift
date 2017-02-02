8a9e17885b61d45a22e7f83ffca46f452c4668da
Jim Avery <jlavery@us.ibm.com>
2017-02-02 15:28:52 -0600

    Updating Kitura dependency

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "AlertNotifications",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0)
     ]
 )
